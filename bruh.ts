ff1d56bb0d6409097a1b9ebcf6633fd38cf7121c
aadilmallick <aadiltj.mallick@gmail.com>
2024-09-24 16:28:13 -0400

    add ytdlp action

--- bruh.ts
+++ bruh.ts
@@ -2,7 +2,11 @@ import { YTDLPModel } from "./src/FFMPEGPathManipulator";
 
 const model = new YTDLPModel();
 async function main() {
-  const stdout = await model.getVersion();
+  const stdout = await model.downloadVideo(
+    "https://www.youtube.com/watch?v=qcrJ3b-mQ6c",
+    "./"
+  );
+  console.log(stdout);
 }
 
 main();
